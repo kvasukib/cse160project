0747544a1650c0bccf8b09f96e9ee24d3869e7b2
douyang <douyang@f3e8c3ac-b22a-7759-8dd3-a6f30dc2a38c>
2011-01-30 03:05:42 +0000

    working parallel version, NYI:imbalanced loads, vertical/horizontal, double check thread boundaries (m+1?)

--- trunk/A2/solve.cpp
+++ trunk/A2/solve.cpp
@@ -13,12 +13,44 @@
 #include <iomanip>
 #include <string>
 #include <math.h>
+#include <pthread.h>
 #include "time.h"
 #include "apf.h"
 #include "types.h"
 using namespace std;
 
+extern pthread_barrier_t barr;
+//global vars
+extern int q, first_q, rest_q; //used to determine partitioning
+extern _DOUBLE_ **E, **R, **E_prev;
+ extern _DOUBLE_ T;
+ extern int m;
+extern  int n;
+extern  int do_stats;
+extern  int plot_freq;
+extern  int tx, ty;
+
+extern  _DOUBLE_ dt;
+extern  _DOUBLE_ alpha;
+
+// This parameter controls the frequncy (in timesteps)
+// that summary statistics are reported
+// Reduce the value of FREQ to increase the frequency,
+// increase the value to raise the frequency
+int STATS_FREQ = 100;
+
+extern ofstream logfile;
+
+extern int NT;
+
+
 void repNorms(ofstream& logfile, _DOUBLE_ **E, _DOUBLE_ t, _DOUBLE_ dt, int m,int n, int niter, int stats_freq);
+
+
+
+
+
+
 
 
 // Reports statistics about the computation: the L2 Norm and the Infinity NOrm
@@ -52,29 +84,37 @@ void repNorms(ofstream& logfile, _DOUBLE_ **E, _DOUBLE_ t, _DOUBLE_ dt, int m,in
      return l2norm;
  }
 
-int solve(ofstream& logfile, _DOUBLE_ ***_E, _DOUBLE_ ***_E_prev, _DOUBLE_ **R, int m, int n, _DOUBLE_ T, _DOUBLE_ alpha, _DOUBLE_ dt, int do_stats, int plot_freq, int stats_freq){
-
  // Simulated time is different from the integer timestep number
  _DOUBLE_ t = 0.0;
  // Integer timestep number
  int niter=0;
 
- _DOUBLE_ **E = *_E, **E_prev = *_E_prev;
+//int solve(ofstream& logfile, _DOUBLE_ ***_E, _DOUBLE_ ***_E_prev, _DOUBLE_ **R, int m, int n, _DOUBLE_ T, _DOUBLE_ alpha, _DOUBLE_ dt, int do_stats, int plot_freq, int stats_freq){
+void * solve_thr (void * arg){
+ 
+
+ //_DOUBLE_ **E = *_E, **E_prev = *_E_prev;
+  int64_t id = reinterpret_cast<int64_t>(arg);
+  int tid = id;
+
+ int start = 1 + id * first_q;
+ int end = start + first_q;
 
  // We continue to sweep over the mesh until the simulation has reached
  // the desired simulation Time
  // This is different from the number of iterations
   while (t<T) {
   
-#ifdef DEBUG
+  if(tid == 0){  
+   #ifdef DEBUG
    printMat(E_prev,m,n);
-   repNorms(logfile,E_prev,t,dt,m,n,niter, stats_freq);
+   repNorms(logfile,E_prev,t,dt,m,n,niter, STATS_FREQ);
    if (plot_freq)
     splot(E_prev,t,niter,m+1,n+1,WAIT);
-#endif
+    #endif
 
-   t += dt;
-   niter++;
+    t += dt;
+    niter++;
 
    /* 
     * Copy data from boundary of the computational box to the
@@ -82,7 +122,7 @@ int solve(ofstream& logfile, _DOUBLE_ ***_E, _DOUBLE_ ***_E_prev, _DOUBLE_ **R, 
     *
     */
     
-   int i,j;
+     int i,j;
      for (j=1; j<=m+1; j++) 
        E_prev[j][0] = E_prev[j][2];
 
@@ -94,10 +134,19 @@ int solve(ofstream& logfile, _DOUBLE_ ***_E, _DOUBLE_ ***_E_prev, _DOUBLE_ **R, 
 
    for (i=1; i<=n+1; i++) 
       E_prev[m+2][i] = E_prev[m][i];
+   }//end first serial section
+
+   int rc = pthread_barrier_wait(&barr);
+   if(rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+   {
+      cerr << "could not wait on barrier\n";
+      exit(-1);
+   }
+
    
    // Solve for the excitation, a PDE
-   for (j=1; j<=m+1; j++){
-     for (i=1; i<=n+1; i++) {
+   for (int j=start; j<end; j++){
+     for (int i=1; i<=n+1; i++) {
 	E[j][i] = E_prev[j][i]+alpha*(E_prev[j][i+1]+E_prev[j][i-1]-4*E_prev[j][i]+E_prev[j+1][i]+E_prev[j-1][i]);
      }
     }
@@ -106,37 +155,48 @@ int solve(ofstream& logfile, _DOUBLE_ ***_E, _DOUBLE_ ***_E_prev, _DOUBLE_ **R, 
     * Solve the ODE, advancing excitation and recovery variables
     *     to the next timtestep
     */
-   for (j=1; j<=m+1; j++){
+   for ( int j=start; j<end; j++){
      _DOUBLE_ *RR = &R[j][1];
      _DOUBLE_ *EE = &E[j][1];
-     for (i=1; i<=n+1; i++, EE++,RR++)
+     for (int i=1; i<=n+1; i++, EE++,RR++)
 	EE[0] += -dt*(kk*EE[0]*(EE[0]-a)*(EE[0]-1)+EE[0]*RR[0]);
    }
 
-   for (j=1; j<=m+1; j++){
+   for (int j=start; j<end; j++){
      _DOUBLE_ *RR = &R[j][1];
      _DOUBLE_ *EE = &E[j][1];
-     for (i=1; i<=n+1; i++, EE++, RR++)
+     for (int i=1; i<=n+1; i++, EE++, RR++)
 	RR[0] += dt*(epsilon+M1* RR[0]/( EE[0]+M2))*(-RR[0]-kk*EE[0]*(EE[0]-b-1));
    }
 
-   if (do_stats)
-     repNorms(logfile, E,t,dt,m,n,niter,stats_freq);
+   //barrier to finish all computations
+   rc = pthread_barrier_wait(&barr);
+   if(rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+   {
+      cerr << "could not wait on barrier\n";
+      exit(-1);
+   }
 
-   if (plot_freq){
-        int k = (int)(t/plot_freq);
-        if ((t-k*plot_freq)<dt){
+
+   if(tid == 0)
+   {
+     if (do_stats)
+       repNorms(logfile, E,t,dt,m,n,niter,STATS_FREQ);
+
+     if (plot_freq){
+          int k = (int)(t/plot_freq);
+          if ((t-k*plot_freq)<dt){
             splot(E,t,niter,m+1,n+1,WAIT);
-        }
-    }
+          }
+      }
 
    // Swap current and previous
-   _DOUBLE_ **tmp = E; E = E_prev; E_prev = tmp;
+     _DOUBLE_ **tmp = E; E = E_prev; E_prev = tmp;
+   }//end serial section
  }
 
   // Store them into the pointers passed in
-  *_E = E;
-  *_E_prev = E_prev;
-
-  return niter;
+  //*_E = E;
+  //*_E_prev = E_prev;
+  return 0;
 }
