3e544e465c49aa343ef9659c60dead23b22f02af
douyang <douyang@f3e8c3ac-b22a-7759-8dd3-a6f30dc2a38c>
2011-02-16 20:58:55 +0000

    parallelized with pthr, still need to fix timer and warmup

--- A3/A3-pthread/elim.cpp
+++ A3/A3-pthread/elim.cpp
@@ -18,10 +18,16 @@ using namespace std;
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
 double getTime();
-
+extern pthread_barrier_t barr;
 extern double **A, **R;
 extern int NT;
 extern int N;
+
+int q;
+int first_q;
+int rest_q;
+
+int totalwork;
 
 void * elim_thr(void * arg)
 {
@@ -31,12 +37,23 @@ void * elim_thr(void * arg)
 // If we get stuck, we can count the number of row swaps
 // Do this for a small matrix
 // int swaps = 0;
-if(tid==0)
-{
 for ( k = 0; k < N; k++ ) {
-//chunk = ceil( (float)(N-k-1)/NT);
-//tid = omp_get_thread_num();
-//totalrows=0;
+
+  if(tid==0)
+  {
+    totalwork = N - (k+1);
+    if(totalwork % NT != 0)
+    {
+      q = totalwork % NT;
+      first_q = ceil( (float) totalwork/NT);
+      rest_q = floor( (float) totalwork/NT);
+    }
+    else
+    {
+      q = NT;
+      first_q = (int) totalwork/NT;
+      
+    }
 /* Partial Pivoting */
       Mx = k;
       for ( i = k+1; i < N; i++ ) {
@@ -51,21 +68,50 @@ for ( k = 0; k < N; k++ ) {
              A[k][j] = t;
          }
       }
+    }
 /* End Partial Pivoting */
-    for ( i = k+1; i < N; i++ ) 
+
+   int rc = pthread_barrier_wait(&barr);
+   if (rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+   {
+      cerr << "could not wait on barrier\n";
+      exit(-1);
+   }
+
+  int start;
+  int end;
+  if(tid < q)
+  {
+    start = k + 1 + id * first_q;
+    end = start + first_q;
+  }
+  else
+  {
+    start = k + 1 + (q*first_q) + (id-q)*rest_q;
+    end = start+ rest_q;
+  }  
+    
+    for ( i = start; i < end; i++ ) 
     {
       A[i][k] /= A[k][k];  
       //totalrows++;
       //printf("Thread %d doing %d\n", tid, i);
     }
-    for ( i = k+1; i < N; i++ ) {
+
+   rc = pthread_barrier_wait(&barr);
+   if (rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+   {
+      cerr << "could not wait on barrier\n";
+      exit(-1);
+   }
+
+    for ( i = start; i < end; i++ ) {
       double Aik = A[i][k];
       double *Ai = A[i];
       for ( j = k+1; j < N; j++ ) 
         Ai[j] -= Aik * A[k][j];
     }  
  // printf("Thread%d did %d rows\n",tid,totalrows);
-  }
 //    cout << "Did " << swaps << " row swaps " << endl;
 }
   pthread_exit(NULL);
